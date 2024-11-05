86e95a447808b9983784b6f37b72f1d5b931e42e
Anthony Mclamb <adylanmclamb@yahoo.com>
2024-11-04 23:41:59 -0500

    reduce wait time

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -16,8 +16,8 @@ const Hero = () => {
         initial={{ opacity: 0.0, y: 40 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{
-          delay: 0.3,
-          duration: 0.8,
+          delay: 0.0,
+          duration: 0.4,
           ease: "easeInOut",
         }}
         className="relative flex flex-col gap-4 px-4 text-white"
